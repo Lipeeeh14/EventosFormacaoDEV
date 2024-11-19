84ed44d851085f637d38ef8a808b97cc12871530
Luiz Felipe <gilberto.barbosa@7comm.com.br>
2024-11-18 21:51:14 -0300

    FEAT[Modulo 1]: Modelagem inicial do frontend

---
+++ apps/frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Pagina from "@/components/template/Pagina";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <Pagina>
+      <div>Início</div>
+    </Pagina>
+  );
+}
